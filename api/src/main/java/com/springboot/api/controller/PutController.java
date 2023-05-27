5c0bf0987ab807161f13b01b8f2048b10905ff21
toobiya <dpqmsxodud@naver.com>
2023-05-28 01:13:47 +0900

    api

---
+++ api/src/main/java/com/springboot/api/controller/PutController.java
@@ -0,0 +1,47 @@
+package com.springboot.api.controller;
+
+import com.springboot.api.dto.MemberDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/put-api")
+public class PutController {
+    // http://localhost:8080/api/v1/put-api/member
+    @PutMapping(value = "/member")
+    public String postMember(@RequestBody Map<String, Object> putData) {
+        StringBuilder sb = new StringBuilder();
+
+        putData.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();           // Body에 입력된 그대로 출력
+    }
+
+    // http://localhost:8080/api/v1/put-api/member1
+    @PutMapping(value = "/member1")
+    public String postMemberDto1(@RequestBody MemberDto memberDto) {
+        return memberDto.toString();    // text/plain으로 일반 문자열 전달
+    }
+
+    // http://localhost:8080/api/v1/put-api/member2
+    @PutMapping(value = "/member2")
+    public MemberDto postMemberDto2(@RequestBody MemberDto memberDto) {
+        return memberDto;               // application/json 형식으로 전달
+    }
+
+    // http://localhost:8080/api/v1/put-api/member3
+    @PutMapping(value = "/member3")
+    public ResponseEntity<MemberDto> postMemberDto3(@RequestBody MemberDto memberDto) {
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)    // 응답코드 202
+                .body(memberDto);               // member2와 같은 형식
+    }
+}
